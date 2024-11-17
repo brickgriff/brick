1b7359c2a0724f45d2e40031bbd651bb2254cc05
bgriffiniv <bgriffiniv@gmail.com>
2024-11-17 08:57:16 -0500

    Update display.js

--- display.js
+++ display.js
@@ -11,14 +11,14 @@ const Display = (function (/*api*/) {
     //ctx.scale(state.zoom,state.zoom);
     //state.zoom=1;
 
-    const gray0="#101010"; // shadow
-    const gray1="#202020"; // background
-    const gray2="#404040"; // inactive
-    const gray3="#808080"; // player
-    const gray4="#c0c0c0"; // active
-    const gray5="#e0e0e0"; // light
+    const gray0="#111111"; // shadow
+    const gray1="#222222"; // background
+    const gray2="#444444"; // inactive
+    const gray3="#888888"; // player
+    const gray4="#cccccc"; // active
+    const gray5="#eeeeee"; // light
 
-    const green="#00b000";
+    const green="#00cc00";
 
     ctx.beginPath();
     ctx.fillStyle=gray1;
@@ -42,7 +42,7 @@ const Display = (function (/*api*/) {
     //ctx.shadowOffsetY=5;
     //ctx.shadowColor=gray0;
     //ctx.strokeStyle="#050505";
-    const r = state.radius;//*state.zoom;//Math.min(500,Math.max(1,state.radius*state.zoom));
+    const r = state.radius*state.minDim/100;//*state.zoom;//Math.min(500,Math.max(1,state.radius*state.zoom));
     ctx.lineWidth=r/5;
     //console.log(state.radius,state.zoom);
     ctx.arc(0,0,r,0,2*Math.PI);
@@ -57,13 +57,13 @@ const Display = (function (/*api*/) {
 
     ctx.beginPath();
     ctx.fillStyle=gray0;
-    ctx.moveTo(200+r,200);
-    ctx.arc(200,200,r,0,2*Math.PI);
+    ctx.moveTo(0.25*state.minDim+r,0.25*state.minDim);
+    ctx.arc(0.25*state.minDim,0.25*state.minDim,r,0,2*Math.PI);
     ctx.fill();
     ctx.beginPath();
     ctx.fillStyle=gray2;
-    ctx.moveTo(r,200);
-    ctx.arc(0,200,r,0,2*Math.PI);
+    ctx.moveTo(r,0.25*state.minDim);
+    ctx.arc(0,0.25*state.minDim,r,0,2*Math.PI);
     ctx.fill();
 
     //ctx.save();
@@ -86,50 +86,72 @@ const Display = (function (/*api*/) {
     ctx.fill();
 
     ctx.save();
-    ctx.beginPath();
+
+    const fill="fill",stroke="stroke";
+    const drawCircle=(args) => ((x,y,r,c,f) => {
+      ctx.beginPath();
+      f===fill?ctx.fillStyle=c:ctx.strokeStyle=c;
+      circle(ctx,x*state.minDim/100,y*state.minDim/100,r*state.minDim/100);
+      ctx[f]();
+    })(...args);
+
+    argsList=[
+      [-15,-25,1,green,stroke],
+      [-17,-26,1,green,stroke],
+      [-16,-28,1,green,stroke],
+      [-10,-30,1,gray0,stroke],
+      [-10,-35,1,gray2,stroke],
+      [-10,-40,1,gray4,stroke],
+      [-10,-45,1,gray5,stroke],
+    ];
+    argsList.forEach(drawCircle);
+
+    //ctx.beginPath();
     //ctx.lineWidth=5; // grass length
-    ctx.strokeStyle=green;
+    //ctx.strokeStyle=green;
     //ctx.shadowOffsetX=5;
     //ctx.shadowOffsetY=5;
     //ctx.shadowColor=gray0;
-    let r1=10;
-    ctx.lineWidth=1; // grass size
-    let x=-100,y=-195;//,h=ctx.lineWidth;
-    ctx.setLineDash([1,(2*Math.PI*r1-10)/10]);
-    ctx.arc(x,y,r1,0,2*Math.PI);
-    ctx.stroke();
+    //let r1=10;
+    //ctx.lineWidth=1; // grass size
+    //let x=-100,y=-195;//,h=ctx.lineWidth;
+    //ctx.setLineDash([1,(2*Math.PI*r1-10)/10]);
+    //ctx.arc(x,y,r1,0,2*Math.PI);
+    //ctx.stroke();
     //ctx.fill();
     //ctx.lineTo(x,y-h);
-    x=-118,y=-212;
+    ///x=-118,y=-212;
     //r1=ctx.lineWidth=7;
-    ctx.setLineDash([1,(2*Math.PI*r1-10)/10]);
-    ctx.beginPath();
+    //ctx.setLineDash([1,(2*Math.PI*r1-10)/10]);
+    //ctx.beginPath();
     //ctx.strokeStyle=gray4;
     //ctx.lineWidth=10;
-    ctx.moveTo(x+r1,y);
-    ctx.arc(x,y,r1,0,2*Math.PI);
-    ctx.stroke();
+    //ctx.moveTo(x+r1,y);
+    //ctx.arc(x,y,r1,0,2*Math.PI);
+    //ctx.stroke();
     //ctx.fill();
     //ctx.lineTo(x,y-h);
-    x=-85,y=-220;
+    //x=-85,y=-220;
     //r1=ctx.lineWidth=10;
-    ctx.setLineDash([1,(2*Math.PI*r1-10)/10]);
-    ctx.beginPath();
+    //ctx.setLineDash([1,(2*Math.PI*r1-10)/10]);
+    //ctx.beginPath();
     //ctx.strokeStyle=gray2;
     //ctx.lineWidth=15;
-    ctx.moveTo(x+r1,y);
-    ctx.arc(x,y,r1,0,2*Math.PI);
+    //ctx.moveTo(x+r1,y);
+    //ctx.arc(x,y,r1,0,2*Math.PI);
     //ctx.fill();
     //ctx.moveTo(x,y);
     //ctx.lineTo(x,y-h);
-    ctx.stroke();
-    ctx.restore();
+    //ctx.stroke();
+    //ctx.restore();
+    //ctx.lineWidth=2;
 
-    ctx.save();
+    // draw clover
+    //ctx.save();
     ctx.beginPath();
     ctx.lineWidth=1;
     ctx.fillStyle=green;
-    r1=5,x=-110,y=-228;
+    let r1=5,x=-110,y=-228;
     //ctx.arc(x,y,r1,0,2*Math.PI);
     let angle1=-Math.PI/2,angle2=30*Math.PI/180,angle3=150*Math.PI/180;
     ctx.arc(x+(r1*Math.cos(angle1)),y+(r1*Math.sin(angle1)),r1-1,0,2*Math.PI);
@@ -148,9 +170,6 @@ const Display = (function (/*api*/) {
     ctx.fill();
     ctx.restore();
 
-
-    // draw clover
-
     // draw roses
 
     // draw mint/plantago
@@ -163,35 +182,14 @@ const Display = (function (/*api*/) {
 
     // draw pokeweed/pigweed/burdock
 
-    ctx.beginPath();
-    ctx.lineWidth=2;
-    ctx.strokeStyle=gray0;
-    ctx.moveTo(-100+r1,-300);
-    ctx.arc(-100,-300,r1,0,2*Math.PI);
-    //ctx.fill();
-    ctx.stroke();
-    ctx.beginPath();
-    //ctx.lineWidth=1;
-    ctx.strokeStyle=gray2;
-    ctx.moveTo(-100+r1,-350);
-    ctx.arc(-100,-350,r1,0,2*Math.PI);
-    //ctx.fill();
-    ctx.stroke();
-    ctx.beginPath();
-    //ctx.lineWidth=1;
-    ctx.strokeStyle=gray4;
-    ctx.moveTo(-100+r1,-400);
-    ctx.arc(-100,-400,r1,0,2*Math.PI);
-    //ctx.fill();
-    ctx.stroke();
-    ctx.beginPath();
-    //ctx.lineWidth=1;
-    ctx.strokeStyle=gray5;
-    ctx.moveTo(-100+r1,-450);
-    ctx.arc(-100,-450,r1,0,2*Math.PI);
-    //ctx.fill();
-    ctx.stroke();
+  };
 
+  const arc = (ctx,x,y,r,start,end) => {
+    ctx.moveTo(x+r,y);
+    ctx.arc(x,y,r,start,end);
+  };
+  const circle = (ctx,x,y,r) => {
+    arc(ctx,x,y,r,0,2*Math.PI);
   };
 
   // return the public api
